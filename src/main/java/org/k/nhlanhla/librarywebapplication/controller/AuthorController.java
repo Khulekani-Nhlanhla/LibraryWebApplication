d2feb66f15be733b9ab55008e361fa057222f57e
Kxxle <64878567+Kxxle@users.noreply.github.com>
2025-04-04 00:06:58 +0200

    I have added path mapping to the AuthorController but only skeletons.

--- src/main/java/org/k/nhlanhla/librarywebapplication/controller/AuthorController.java
+++ src/main/java/org/k/nhlanhla/librarywebapplication/controller/AuthorController.java
@@ -1,5 +1,19 @@
 package org.k.nhlanhla.librarywebapplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
 public class AuthorController {
 
+    @GetMapping("/{id}")
+
+    @GetMapping("/{id}/author")
+
+    @PostMapping
+
+    @PutMapping("{id}")
+
+    @DeleteMapping("{id}")
 }
